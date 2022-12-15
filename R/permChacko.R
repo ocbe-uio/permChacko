0e5dba92c17d263a0c21304431e241b74b3e4bc2
Waldir Leoncio <w.l.netto@medisin.uio.no>
2022-12-15 13:55:12 +0100

    Added ordering process

--- R/permChacko.R
+++ R/permChacko.R
@@ -4,19 +4,31 @@
 #' Chacko, V. J. (1966). Modified chi-square test for ordered alternatives.
 #' Sankhyā: The Indian Journal of Statistics, Series B, 185-190.
 #' @export
-permChacko <- function(x) {
+permChacko <- function(x, verbose = FALSE) {
   # TODO: implement original algo following Graeme's notes:
   #
   # We begin with a list of k values x1,…xk with associated weights t1 = t2  =
   # …. = tk = 1.
-  #
+  x_t <- cbind("x" = x, "t" = x ^ 0L)
+
   # If for any 1≤i ≤ (k-1), we have that xk > xk+1 then we replace both values
   # xk and xk+1 by a single value which is their weighted average (using the
-  # weights tk and tk+1), this new value takes the combined weight of the two
+  # weights tk and tk+1),
+  # this new value takes the combined weight of the two
   # values it replaces tk + tk+1. The list is now one shorter, so k becomes k-1
-  #
+
   # We repeat this process until k = 1 or we have a monotone increasing sequence
   # of numbers.
+
+  while (nrow(x_t) > 1L && any(diff(x_t[, "x"]) < 0L)) {
+    if (verbose) {
+      message("Reordering vector")
+      print(x_t)
+    }
+    x_t <- orderingProcess(x_t, verbose = verbose)
+  }
+
+  return(x_t)
   #
   # This procedure allows me to recreate the two ordering process examples shown
   # on pages 187 and 189 of Chacko (1966).
@@ -56,4 +68,30 @@ permChacko <- function(x) {
   # procedure and calculate the test statistic according to equation 5. The
   # p-value is simply the fraction of such permutations that yield a test
   # statistic equal to or greater than the one we originally observed.
+}
+
+orderingProcess <- function(x_t, verbose = verbose) {
+  x <- x_t[, "x"]
+  t <- x_t[, "t"]
+  i <- 1L
+  while (i <= length(x)) {
+    if (verbose) cat("Evaluating index", i, "\n")
+    if (x[i] > x[i + 1L]) {
+      if (verbose) message("Replacing ", x[i], " and ", x[i + 1L])
+      # Replacing values
+      x[i] <- weighted.mean(c(x[i], x[i + 1L]), c(t[i], t[i + 1L]))
+      t[i] <- t[i] + t[i + 1L]
+
+      # Updating vector
+      x <- x[-(i + 1L)]
+      t <- t[-(i + 1L)]
+
+      if (verbose) {
+        message("New values and weights")
+        print(cbind("x" = x, "t" = t))
+      }
+    }
+    i <- i + 1L
+  }
+  return(cbind("x" = x, "t" = t))
 }
