dbd5b9205cb86f4059cf10b32142bb4f7b5bff8d
Waldir Leoncio <w.l.netto@medisin.uio.no>
2022-12-15 14:07:30 +0100

    Fixed loop condition

--- R/permChacko.R
+++ R/permChacko.R
@@ -74,7 +74,7 @@ orderingProcess <- function(x_t, verbose = FALSE) {
   x <- x_t[, "x"]
   t <- x_t[, "t"]
   i <- 1L
-  while (i <= length(x)) {
+  while (i < length(x)) {
     if (verbose) cat("Evaluating index", i, "\n")
     if (x[i] > x[i + 1L]) {
       if (verbose) message("Replacing ", x[i], " and ", x[i + 1L])
---
+++ tests/testthat/test-chackoPapers.R
@@ -0,0 +1,7 @@
+test_that("Examples from Chacko (1966) produce correct vectors", {
+  chacko66_3 <- c(10L, 16L, 14L, 12L, 18L)
+  chacko66_5 <- c(12L, 14L, 18L, 16L, 22L, 20L, 18L, 24L, 26L, 30L)
+  expect_equal(permChacko(chacko66_3)[, "x"], c(10, 14, 18))
+  expect_equal(permChacko(chacko66_3)[, "t"], c(1, 3, 1))
+  expect_equal(permChacko(chacko66_5)[, "x"], c(12, 14, 17, 20, 24, 26, 30))
+})
