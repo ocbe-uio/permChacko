53085b7658a43355fb84efe40a47d951dde5acba
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-01-17 10:30:30 +0100

    Adjusted tests to 1f32cb

--- tests/testthat/test-chackoPapers.R
+++ tests/testthat/test-chackoPapers.R
@@ -5,6 +5,6 @@ test_that("Examples from Chacko (1966) produce correct vectors", {
   expect_equal(reduceVector(chacko66_3)[, "t"], c(1, 3, 1))
   expect_equal(reduceVector(chacko66_5)[, "x"], c(12, 14, 17, 20, 24, 26, 30))
   expect_equal(reduceVector(chacko66_5)[, "t"], c(1, 1, 2, 3, 1, 1, 1))
-  expect_equal(permChacko(chacko66_3), 2.285714, tolerance = 1e-4)
-  expect_equal(permChacko(chacko66_5), 13.5)
+  expect_equal(permChacko(chacko66_3)[["chisq_bar"]], 2.28571, tolerance = 1e-4)
+  expect_equal(permChacko(chacko66_5)[["chisq_bar"]], 13.5)
 })
