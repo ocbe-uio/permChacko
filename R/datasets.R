421741357350559f806bbc1cdf23d102105cbbd2
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-01-17 13:27:18 +0100

    Documented datasets

--- R/datasets.R
+++ R/datasets.R
@@ -1,3 +1,11 @@
+#' @title Table I
+#' @description Table of \eqn{p_{m, k}}. Gives the values of \eqn{p_{m, k}} for
+#' equal sample sizes and \eqn{k = 3, 4, \ldots, 10}.
+#' @references
+#' Chacko, V. J. (1963). Testing homogeneity against ordered alternatives. The
+#' Annals of Mathematical Statistics, 945-956.
+#' @name chacko_1963_table_1
+#' @docType data
 chacko_1963_table_1 <- matrix(
   c(
     .333333, .500000, .166667, rep(NA, 7),
@@ -13,6 +21,32 @@ chacko_1963_table_1 <- matrix(
   ncol = 8,
   dimnames = list(paste("m =", seq_len(10)), paste("k =", seq(3, 10)))
 )
-chacko66_3 <- c(10L, 16L, 14L, 12L, 18L)
-chacko66_5 <- c(12L, 14L, 18L, 16L, 22L, 20L, 18L, 24L, 26L, 30L)
+
+#' @title Chacko (1966), section 3
+#' @description A multinomial example with 5 cell frequencies
+#' @references
+#' Chacko, V. J. (1966). Modified chi-square test for ordered alternatives.
+#' Sankhyā: The Indian Journal of Statistics, Series B, 185-190.
+#' @name chacko66_3
+#' @docType data
+chacko66_3 <- c("x1" = 10L, "x2" = 16L, "x3" = 14L, "x4" = 12L, "x5" = 18L)
+
+#' @title Chacko (1966), section 5
+#' @description A plate with the humidity values continuously decreasing was
+#' divided into 10 equal parts and 20 termites introduced on each part. The
+#' number of termites counted as a specified time interval on each of the 10
+#' parts of the plate are shown in the dataset
+#' @references
+#' Chacko, V. J. (1966). Modified chi-square test for ordered alternatives.
+#' Sankhyā: The Indian Journal of Statistics, Series B, 185-190.
+#' @name chacko66_3
+#' @docType data
+chacko66_5 <- c(
+  "x1" = 12L, "x2" = 14L, "x3" = 18L, "x4" = 16L, "x5" = 22L,
+  "x6" = 20L, "x7" = 18L, "x8" = 24L, "x9" = 26L, "x10" = 30L
+)
+
+#' @title Example by Graeme Ruxton
+#' @name ruxton22_12_07
+#' @docType data
 ruxton22_12_07 <- c(6, 8, 4, 7, 3)
