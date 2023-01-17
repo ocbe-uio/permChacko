086e5531c6489ee85f02aaca62d4f2c8e4037811
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-01-17 09:42:29 +0100

    Vectorized for loops

--- R/permChacko.R
+++ R/permChacko.R
@@ -52,27 +52,27 @@ permChacko <- function(x, n_perm = 1000L, verbose = FALSE) {
   # I think with the computing power now we would simply obtain the p-value
   # using a permutation test.
 
-  # TODO: implement permutation
   # That is – imagine that the sum of our original K values is N, then by a
   # permutation I mean a stochastic distribution of N objects (independently)
   # across the k categories (with each category being equally likely under the
   # null hypothesis).
-  perm_chisq_bar_larger_than_chisq_bar <- 0L
-  for (rep in seq_len(n_perm)) {
-    perm_x <- permutateX(x)
+  perm_chisq_bar_larger_than_chisq_bar <- vapply(
+    X = seq_len(n_perm),
+    FUN = function(n, x, k) {
+      perm_x <- permutateX(x)
 
-    # For each such permutation we can go through the ordering procedure and
-    # calculate the test statistic according to equation 5.
-    perm_x_t <- reduceVector(perm_x, verbose)
-    perm_chisq_bar <- chackoStatistic(perm_x_t, n = sum(x), k)
-
-    if (perm_chisq_bar >= chisq_bar) {
-      perm_chisq_bar_larger_than_chisq_bar <- perm_chisq_bar_larger_than_chisq_bar + 1
-    }
-  }
+      # For each such permutation we can go through the ordering procedure and
+      # calculate the test statistic according to equation 5.
+      perm_x_t <- reduceVector(perm_x, verbose)
+      perm_chisq_bar <- chackoStatistic(perm_x_t, n = sum(x), k)
+      return(perm_chisq_bar >= chisq_bar)
+    },
+    FUN.VALUE = vector("logical", 1L),
+    x = x, k = k
+  )
   # The p-value is simply the fraction of such permutations that yield a test
   # statistic equal to or greater than the one we originally observed.
-  perm_p_value <- perm_chisq_bar_larger_than_chisq_bar / n_perm
+  perm_p_value <- sum(perm_chisq_bar_larger_than_chisq_bar) / n_perm
 
   return(c("chisq_bar" = chisq_bar, "p-value" = perm_p_value))
 }
--- R/permutateX.R
+++ R/permutateX.R
@@ -1,10 +1,6 @@
 permutateX <- function(X) {
   N <- sum(X)
   K <- length(X)
-  perm_x <- vector("integer", length = K)
-  for (x in seq_len(N)) {
-    x_category <- sample(K, size = 1)
-    perm_x[x_category] <- perm_x[x_category] + 1
-  }
+  perm_x <- unname(table(sample(K, size = N, replace = TRUE)))
   return(perm_x)
 }
