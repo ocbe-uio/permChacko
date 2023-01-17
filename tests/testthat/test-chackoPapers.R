997b5652690bbc35994999afed4020d5dc827767
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-01-17 12:05:03 +0100

    Added test units

--- tests/testthat/test-chackoPapers.R
+++ tests/testthat/test-chackoPapers.R
@@ -7,4 +7,15 @@ test_that("Examples from Chacko (1966) produce correct vectors", {
   expect_equal(reduceVector(chacko66_5)[, "t"], c(1, 1, 2, 3, 1, 1, 1))
   expect_equal(permChacko(chacko66_3)[["chisq_bar"]], 2.28571, tolerance = 1e-4)
   expect_equal(permChacko(chacko66_5)[["chisq_bar"]], 13.5)
+  set.seed(3174)
+  expect_equal(
+    permChacko(chacko66_3)[["numeric_p-value"]],
+    permChacko(chacko66_3)[["tabular_p-value"]],
+    tolerance = 1e-1
+  )
+  expect_equal(
+    permChacko(chacko66_5)[["numeric_p-value"]],
+    permChacko(chacko66_5)[["tabular_p-value"]],
+    tolerance = 1e-1
+  )
 })
--- tests/testthat/test-customExamples.R
+++ tests/testthat/test-customExamples.R
@@ -3,4 +3,10 @@ test_that("Other from produce correct vectors", {
   expect_equal(reduceVector(ruxton22_12_07)[, "x"], c("x" = 5.6))
   expect_equal(reduceVector(ruxton22_12_07)[, "t"], c("t" = 5))
   expect_equal(permChacko(ruxton22_12_07)[["chisq_bar"]], 0)
+  set.seed(2715249)
+  expect_equal(
+    permChacko(ruxton22_12_07)[["numeric_p-value"]],
+    permChacko(ruxton22_12_07)[["tabular_p-value"]],
+    tolerance = 1e-0
+  )
 })
