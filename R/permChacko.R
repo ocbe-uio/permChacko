136fcc43a6c6d85279d0c8ef42d4ba5cb6ce1265
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-05-19 06:46:31 +0200

    Reorganized comments
    Perhaps Graeme's notes make a comeback as a vignettegit cmm 'Improved package description

--- R/permChacko.R
+++ R/permChacko.R
@@ -24,38 +24,7 @@ permChacko <- function(x, n_perm = 1000L, verbose = FALSE) {
   k <- length(x)
   chisq_bar <- chackoStatistic(x_t, n = sum(x), k)
 
-  # Notice that Chacko was entirely comfortable with this ordering process
-  # ending with a single value. If you look at their table on page 188 then he
-  # suggests that under the null hypothesis – if you start with a list of 5
-  # values, then you have a 20% chance that this process that the order process
-  # results in a single value.
-  #
-  # Even if the outcome is a single value, the test statistic can be calculated
-  # from equation 5 on page 188.
-  #
-  # The question then becomes how do we obtain a p-value associated with this
-  # test statistic.
-  #
-  # I agree that just under equation 5, Chacko says that the test statistic is
-  # asymptotically chi-squared with m- 1 degrees of freedom (where m is the
-  # length of the final order list of values). However, he does not discuss what
-  # this means in the event on m = 1. However, as I discuss above, they clearly
-  # expect that this will happen sometimes.
-  #
-  # If you look at their final example on how they evaluate significance – they
-  # say that you reject the null hypothesis if the  observed calculated value
-  # obtained from equation (5) is greater than the value c obtained from
-  # equation 6. This approach does work when m = 1 but seems very cumbersome –
-  # requiring calculation of the probability values given on the table on page
-  # 188, which requires consultation of Chacko (1963).
-  #
-  # I think with the computing power now we would simply obtain the p-value
-  # using a permutation test.
-
-  # That is – imagine that the sum of our original K values is N, then by a
-  # permutation I mean a stochastic distribution of N objects (independently)
-  # across the k categories (with each category being equally likely under the
-  # null hypothesis).
+  # Calculating the mean of the current permutation
   perm_chisq_bar <- vapply(
     X = seq_len(n_perm),
     FUN = function(n, x, k) {
@@ -70,6 +39,7 @@ permChacko <- function(x, n_perm = 1000L, verbose = FALSE) {
     FUN.VALUE = vector("double", 1L),
     x = x, k = k
   )
+
   # The p-value is simply the fraction of such permutations that yield a test
   # statistic equal to or greater than the one we originally observed.
   perm_p_value <- sum(perm_chisq_bar >= chisq_bar) / n_perm
