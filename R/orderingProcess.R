f3c98f189ff728449f1ba79c96b9222591d439a0
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-08-01 07:59:55 +0200

    Moved messages to orderingProcess (#3)
    Concentrating verbose messages on the same function makes more sense while also facilitating development

--- R/orderingProcess.R
+++ R/orderingProcess.R
@@ -2,10 +2,16 @@ orderingProcess <- function(x_t, verbose = FALSE) {
   x <- x_t[, "x"]
   t <- x_t[, "t"]
   i <- 1L
+  if (verbose) {
+    message("Reordering vector")
+    print(x_t)
+  }
   while (i < length(x)) {
-    if (verbose) cat("Evaluating index", i, "\n")
-    if (x[i] > x[i + 1L]) {
-      if (verbose) message("Replacing ", x[i], " and ", x[i + 1L])
+    if (verbose) cat("Comparing", x[i], "and", x[i + 1L])
+    if (x[i] <= x[i + 1L]) {
+      if (verbose) cat(". Next.\n")
+    } else {
+      if (verbose) cat(". Replacing.\n")
       # Replacing values
       x[i] <- weighted.mean(c(x[i], x[i + 1L]), c(t[i], t[i + 1L]))
       t[i] <- t[i] + t[i + 1L]
@@ -21,5 +27,10 @@ orderingProcess <- function(x_t, verbose = FALSE) {
     }
     i <- i + 1L
   }
-  return(cbind("x" = x, "t" = t))
+  if (verbose) {
+    message("Final vector")
+    out <- cbind("x" = x, "t" = t)
+    print(out)
+  }
+  return(out)
 }
--- R/reduceVector.R
+++ R/reduceVector.R
@@ -7,15 +7,7 @@
 reduceVector <- function(x, verbose = FALSE) {
   x_t <- cbind("x" = unname(x), "t" = unname(x) ^ 0L)
   while (nrow(x_t) > 1L && isMonotoneIncreasing(x_t[, "x"])) {
-    if (verbose) {
-      message("Reordering vector")
-      print(x_t)
-    }
     x_t <- orderingProcess(x_t, verbose = verbose)
   }
-  if (verbose) {
-    message("Final vector")
-    print(x_t)
-  }
-  return(x_t)
+  return(invisible(x_t))
 }
