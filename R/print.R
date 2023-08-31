3a55a9749c283dbd20b7b895d610e3adb8d6f5ee
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-08-31 14:26:56 +0200

    Created print method for reduceVector() (#7)

--- R/print.R
+++ R/print.R
@@ -20,3 +20,26 @@ print.chacko_test <- function(x, ...) {
     )
   )
 }
+
+#' @export
+print.reduced_vector <- function(x, details = TRUE, ...) {
+  if (x[["verbose"]] >= 1L) {
+    cat(sprintf("Original vector has been reduced %d times", x[["reductions"]]))
+  } else {
+    cat(
+      sprintf(
+        paste0(
+          "Original vector: %s\n",
+          "Reduced vector : %s\n",
+          "Final weights  : %s\n",
+          "Original vector has been reduced %d times\n\n",
+          "Run reduceVector() with verbosity > 1) to see the reduction process"
+        ),
+        paste0(x[["original_vector"]], collapse = "\t"),
+        paste0(x[["reduced_vector"]], collapse = "\t"),
+        paste0(x[["weights"]], collapse = "\t"),
+        x[["reductions"]]
+      )
+    )
+  }
+}
