526aa1eb612012e57d0ae23ce3056953b784a11f
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-09-05 10:02:50 +0200

    Implemented faster `chackoStatistic()`

--- R/chackoStatistic.R
+++ R/chackoStatistic.R
@@ -1,10 +1,14 @@
-chackoStatistic <- function(x_t, n, k) {
+chackoStatistic <- function(x_t, n, k, fast = FALSE) {
   x_bar <- x_t[, "x"]
   t <- x_t[, "t"]
   m <- length(unique(x_bar))
   power_sum <- 0
   for (j in seq_len(m)) {
     power_sum <- power_sum + t[[j]] * (x_bar[[j]] - n / k) ^ 2
+  }
+  if (fast) {
+    x_t_unique <- x_t[!duplicated(x_t[, "x"]), , drop = FALSE]
+    return(k / n * sum(x_t_unique[, "t"] * (x_t_unique[, "x"] - n / k) ^ 2))
   }
   return(k / n * power_sum)
 }
