652a6b2fc7c3f136c4d977be2305429f403c8202
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-09-05 11:49:42 +0200

    Added TODOs

--- R/chackoStatistic.R
+++ R/chackoStatistic.R
@@ -1,13 +1,14 @@
 chackoStatistic <- function(x_t, n, k, fast = FALSE) {
+  # TODO: supersede fast = FALSE with fast = TRUE
   x_bar <- x_t[, "x"]
   t <- x_t[, "t"]
-  m <- length(unique(x_bar))
+  m <- length(unique(x_bar)) # TODO: Check what Chacko means by "unique"
   power_sum <- 0
   for (j in seq_len(m)) {
     power_sum <- power_sum + t[[j]] * (x_bar[[j]] - n / k) ^ 2
   }
   if (fast) {
-    x_t_unique <- x_t[!duplicated(x_t[, "x"]), , drop = FALSE]
+    x_t_unique <- x_t[!duplicated(x_t[, "x"]), , drop = FALSE] # TODO: Check what Chacko means by "unique"
     return(k / n * sum(x_t_unique[, "t"] * (x_t_unique[, "x"] - n / k) ^ 2))
   }
   return(k / n * power_sum)
